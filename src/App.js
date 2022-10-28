7932bd414abe2fb740b1d1b1abf83dcdb5f01098
Karanjot1995 <karan.nanda97@gmail.com>
2022-10-28 02:06:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ function App() {
   const [data, setData] = useState({})
   
   async function fetchData(){
-    const url = `http://localhost:3001/`;
-    const res = await axios.get(url);
-    console.log(res.data)
+    const url = `http://34.170.76.69/`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => console.log(err))
+    // const res = await axios.get(url);
+    // console.log(res.data)
   }
   useEffect(() => {
     fetchData()
