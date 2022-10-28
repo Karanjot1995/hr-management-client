a7359a936419e33775dba841680cb48712f756a4
Karanjot Singh <ksingh@megger.net>
2022-10-27 23:35:03 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState, useReducer, useEffect} from "react";
+import axios from 'axios';
 
 function App() {
+  const [state, setState] = useState({name:''})
+  const [data, setData] = useState({})
+  
+  async function fetchData(){
+    const url = `http://localhost:3001/`;
+    const res = await axios.get(url);
+    console.log(res.data)
+  }
+  useEffect(() => {
+    fetchData()
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Test App
     </div>
   );
 }
