0b241885f0e2158308e857bd97f3f376221aadf1
Karanjot1995 <karan.nanda97@gmail.com>
2022-10-28 15:24:10 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ function App() {
   const [data, setData] = useState({})
   
   async function fetchData(){
-    const url = `http://34.170.76.69/`;
-    fetch('https://8080-cs-1053727390227-default.cs-us-central1-pits.cloudshell.dev')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => console.log(err))
+    const url = `https://hr-server-366909.uc.r.appspot.com`;
+    const headers = {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
+    const res = await axios(url)
+    console.log(res.data)
+    // fetch('https://8080-cs-1053727390227-default.cs-us-central1-pits.cloudshell.dev')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data)
+    //   })
+    //   .catch(err => console.log(err))
     // const res = await axios.get(url);
     // console.log(res.data)
   }
